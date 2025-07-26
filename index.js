49f0c4c34fc96c35e2054dd61fdec906dfe9eee9
Danielle Morvant <daniellemorvant1@gmail.com>
2025-07-25 22:17:08 -0500

    stringUtils.js troubleshoot function

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { mathPower, squareRooter } from "./mathUtils.js" 
+import { randomGreeting } from "./stringUtils.js";
 import chalk from "chalk";
 
 console.log(chalk.bgCyanBright("index.js works :)"));
 
 console.log(mathPower(4,4));
 console.log(chalk.blueBright(squareRooter(79)));
+console.log(randomGreeting[0])
