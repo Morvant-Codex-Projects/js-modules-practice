23b0da0a9521a476dd188cd6d3ae465ca9b0bf65
Danielle Morvant <daniellemorvant1@gmail.com>
2025-07-26 09:45:57 -0500

    wrote export of nonworking function...

--- stringUtils.js
+++ stringUtils.js
@@ -5,11 +5,21 @@ console.log(chalk.bgMagentaBright("lets play with a string"));
 
 const greetings = ["hey yall","Hola","hello","whats up doc?","bonjour"];
 
-
-function capitlizeString(){
+/**
+ * 
+ * @param {[string]}
+ * @returns {[string]} returns each greeting capitilized
+ * 
+ */
+export function capitlizeString(){
+    return greetings.capitlizeString(String.forEach(element => {
+    
+}));
     
 }
 console.log((greetings) + " its a beautiful day in the neighborhood");
+
+
     
 
 
