13fc4b931f8b3a85d8dc9c5c0ec3a67c79465d3b
Danielle Morvant <daniellemorvant1@gmail.com>
2025-07-26 09:49:25 -0500

    corrected chasnged name of import for stringUtils.js in index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { mathPower, squareRooter } from "./mathUtils.js" 
-import { randomGreeting } from "./stringUtils.js";
+import { capitilizeString } from "./stringUtils.js";
 import chalk from "chalk";
 
 console.log(chalk.bgCyanBright("index.js works :)"));
+
+console.log
 
 console.log(mathPower(4,4));
 console.log(chalk.blueBright(squareRooter(79)));
