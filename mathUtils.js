1c31d6134f5a525e2677289d84ec5e6ed5f16a66
Danielle Morvant <daniellemorvant1@gmail.com>
2025-07-25 19:57:28 -0500

    function addedand working

--- mathUtils.js
+++ mathUtils.js
@@ -1,15 +1,8 @@
-import  ("math.js");
-
 console.log("lets do the math!");
 
 
+export function mathPower(a,b) {
+    return Math.pow(a,b);
+}
 
-const numbers = [2,4,6,8,14,16,18,20,31];
-const maxValue = math.max(numbers);
-console.log('Maximum Value:', maxValue);
-const minValue = math.min(numbers);
-console.log('Minimum Value:', minValue);
-const averageValue = math.mean(numbers);
-console.log('Average Value:', averageValue);
-const sumValue = math.sum(numbers);
-console.log('Sum:', sumValue);
+console.log(mathPower(4,8));
