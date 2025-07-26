00f54baaee4b4b71cb650323112d669fbf1c8bf7
Danielle Morvant <daniellemorvant1@gmail.com>
2025-07-25 20:44:12 -0500

    working on function to be called

--- stringUtils.js
+++ stringUtils.js
@@ -1,1 +1,9 @@
+import { random } from "math.js/lib/probability";
+
 console.log("lets find a random object from a string");
+
+const greetings =["hey yall", "hello", "bonjour", "hola"];
+
+export function randomGreeting(greetings) {
+    const randomNumber = _.random(0,)
+}
