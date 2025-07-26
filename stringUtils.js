511d1436db4fa0d3a49fe240d73aa82d47567369
Danielle Morvant <daniellemorvant1@gmail.com>
2025-07-26 01:01:53 -0500

    troubleshooting

--- stringUtils.js
+++ stringUtils.js
@@ -1,11 +1,18 @@
 import chalk, { chalkStderr } from "chalk";
+import _ from "lodash.js";
 
-console.log(chalk.bgMagentaBright("lets find a random object from a string"));
+console.log(chalk.bgMagentaBright("lets play with a string"));
 
 const greetings = ["hey yall","Hola","hello","whats up doc?","bonjour"];
 
-export function toUpperCase(str) {
-    return str.toUpperCase();
-}
 
-console.log(toUpperCase());
+function capitlizeString(){
+    
+}
+console.log((greetings) + " its a beautiful day in the neighborhood");
+    
+
+
+
+
+
